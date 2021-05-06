8ba004008db032ccbd61556b9de941452624b34b
pallavi-gm98 <pallavishivas96@gmail.com>
2021-05-06 16:33:47 +0530

    Modified

---
+++ app/src/main/java/com/example/airlineapp/TravellerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.airlineapp;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TravellerAdapter extends RecyclerView.Adapter<TravellerAdapter.MyViewHolder> {
+    @NonNull
+    @Override
+    public TravellerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TravellerAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
